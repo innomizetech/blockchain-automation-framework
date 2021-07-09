48136993c11dbcc63f4c01e39f7b9755eb94a3d7
hoang-innomizetech <hoang@innomizetech.com>
2021-07-09 10:13:45 +0700

    [ci skip] Pushing deployment files for namespace, service accounts and clusterrolebinding
    Signed-off-by: hoang-innomizetech <hoang@innomizetech.com>

--- vagrant/join.sh
+++ vagrant/join.sh
@@ -1,2 +1,2 @@
-kubeadm join 10.0.0.10:6443 --token n22xcw.lx2huly9dbef7j1i \
-	--discovery-token-ca-cert-hash sha256:9ab0331e79568895f89671eea8ac6120a7d5fe0c3e11b9574be9b0dcf63a7e33 
+kubeadm join 10.0.0.10:6443 --token aip7zg.ecx7wu38o7jz55xr \
+	--discovery-token-ca-cert-hash sha256:dd74483e1eafd89137ed355d23d673b3b628a0cfdbfeafe8046335c057e39791 
